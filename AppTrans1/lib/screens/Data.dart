f679c2f52bb9ab0ab269e216cce0fd59744ff2f0
MorNdour <ndmor@ept.sn>
2022-09-10 09:07:20 +0000

    flutterApp

---
+++ AppTrans1/lib/screens/Data.dart
@@ -0,0 +1,5 @@
+
+class Data {
+  String imgUrl, temperature, humidite, maladie_atteinte, date;
+  Data(this.imgUrl, this.temperature, this.humidite,this.date,this.maladie_atteinte);
+}
---
+++ AppTrans1/lib/widgets/auth/auth_form.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class AuthForm extends StatefulWidget {
+  const AuthForm(this.submitFn, this.isLoading, {Key key}) : super(key: key);
+
+  final bool isLoading;
+  final void Function(
+    String email,
+    String password,
+    String userName,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState.save();
+      widget.submitFn(_userEmail.trim(), _userPassword.trim(), _userName.trim(),
+          _isLogin, context);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/bgImage4.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: ListView(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(
+                        left: 25.0, right: 125.0, top: 70, bottom: 0),
+                    child: DefaultTextStyle(
+                      style: const TextStyle(
+                        fontFamily: "Raleway",
+                        fontSize: 70.0,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 37, 69, 40),
+                      ),
+                      child: AnimatedTextKit(
+                        animatedTexts: [
+                          WavyAnimatedText("Sign In"),
+                        ],
+                        isRepeatingAnimation: true,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Form(
+                key: _formKey,
+                child: SingleChildScrollView(
+                  child: Container(
+                      padding: const EdgeInsets.only(
+                          top: 50.0, left: 10.0, right: 30.0),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            key: const ValueKey('email'),
+                            decoration: const InputDecoration(
+                                labelText: 'Email',
+                                labelStyle: TextStyle(
+                                    fontFamily: 'RobotoCondensed',
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.green),
+                                )),
+                            keyboardType: TextInputType.emailAddress,
+                            autocorrect: false,
+                            textCapitalization: TextCapitalization.none,
+                            enableSuggestions: false,
+                            validator: (value) {
+                              if (value.isEmpty || !value.contains('@')) {
+                                return 'Please enter a valid email address';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _userEmail = value;
+                            },
+                          ),
+                          if (!_isLogin) const SizedBox(height: 10.0),
+                          if (!_isLogin)
+                            TextFormField(
+                              key: const ValueKey('username'),
+                              decoration: const InputDecoration(
+                                  labelText: 'Username',
+                                  labelStyle: TextStyle(
+                                      fontFamily: 'RobotoCondensed',
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey),
+                                  focusedBorder: UnderlineInputBorder(
+                                      borderSide:
+                                          BorderSide(color: Colors.green))),
+                              //autocorrect: false,
+                              textCapitalization: TextCapitalization.words,
+                              enableSuggestions: false,
+                              validator: (value) {
+                                if (value.isEmpty) {
+                                  return 'Please enter a username';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _userName = value;
+                              },
+                              // ignore: missing_return
+                            ),
+                          const SizedBox(height: 10.0),
+                          TextFormField(
+                            key: const ValueKey('password'),
+                            decoration: const InputDecoration(
+                              labelText: 'Password',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'RobotoCondensed',
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey),
+                              focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.green)),
+                            ),
+                            obscureText: true,
+
+                            // ignore: missing_return
+                            validator: (value) {
+                              if (value.isEmpty || value.length < 7) {
+                                return 'Please enter a long password';
+                              }
+                              return null;
+                            },
+                            onSaved: (value) {
+                              _userPassword = value;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 35,
+                          ),
+                          if (widget.isLoading)
+                            const CircularProgressIndicator(),
+                          if (!widget.isLoading)
+                            SizedBox(
+                              height: 45.0,
+                              width: 200,
+                              child: Material(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(30)),
+                                //shadowColor: Colors.black,
+                                color: const Color.fromARGB(255, 36, 78, 37)
+                                    .withOpacity(0.3),
+                                elevation: 30.0,
+                                child: TextButton(
+                                  style: ButtonStyle(
+                                      shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                          const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(20)),
+                                              side: BorderSide(
+                                                  color: Colors.green)))),
+                                  onPressed: _trySubmit,
+                                  child: Center(
+                                    child: Text(
+                                      _isLogin ? "Log In" : "Sign up",
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Raleway'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          const SizedBox(
+                            height: 25,
+                          ),
+                          if (!widget.isLoading)
+                            SizedBox(
+                              height: 140.0,
+                              child: Material(
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(25)),
+                                color: Colors.transparent,
+                                child: TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      _isLogin = !_isLogin;
+                                    });
+                                  },
+                                  child: Container(
+                                    padding: const EdgeInsets.only(
+                                        top: 10.0, left: 0.0, right: 170.0),
+                                    child: Text(
+                                      _isLogin
+                                          ? "New User? Create a new account!"
+                                          : "Already User? Log in!",
+                                      style: const TextStyle(
+                                          color:
+                                              Color.fromARGB(255, 52, 125, 65),
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                          fontFamily: 'Raleway'),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          Padding(
+                              padding: EdgeInsets.only(
+                                  bottom: MediaQuery.of(context)
+                                      .viewInsets
+                                      .bottom)),
+                        ],
+                      )),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
+  }
+}
