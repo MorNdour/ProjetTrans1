318f880949551c9821ac0e57cdb74eaa0fd1d98a
MorNdour <98852373+MorNdour@users.noreply.github.com>
2022-09-11 15:08:19 +0000

    Delete AppTrans1 directory

--- AppTrans1/android/app/src/main/kotlin/com/example/authentifaction_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.authentifaction_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- AppTrans1/lib/screens/Data.dart
+++
@@ -1,5 +0,0 @@
-
-class Data {
-  String imgUrl, temperature, humidite, maladie_atteinte, date;
-  Data(this.imgUrl, this.temperature, this.humidite,this.date,this.maladie_atteinte);
-}
